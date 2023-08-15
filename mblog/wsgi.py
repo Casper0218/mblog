94c1d27a6b0eb1e0d2138ba7c7b7df8b93f59976
Casper0218 <casper50206@gmail.com>
2023-08-15 11:13:47 +0800

    ver1.00.04

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -1,15 +1,12 @@
-"""
-WSGI config for mblog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# 專案資料夾/mblog/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
+import sys
+
+path = "/home/casper50206/mblog/"
+
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mblog.settings")
 
