65ab2f06c079bbb357a39bcb611de7cd57236f99
Casper0218 <casper50206@gmail.com>
2023-09-11 12:12:56 +0800

    Final 1.01

--- myAssistant/wsgi.py
+++ myAssistant/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = "/home/casper0218/django_env/lib/python3.10/site-packages"
+path = "/home/casper50206/django_env/lib/python3.10/site-packages"
 if path not in sys.path:
     sys.path.insert(0, path)
 path = "/home/casper50206/mblog"
