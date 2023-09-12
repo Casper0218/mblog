c4f78a69f04d175cf3930464520c9f1d7ee27279
Casper0218 <casper50206@gmail.com>
2023-09-12 10:52:13 +0800

    Final_1.1.1

--- myAssistant/wsgi.py
+++ myAssistant/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
 
 path = "/home/casper50206/django_env/lib/python3.10/site-packages"
 if path not in sys.path:
@@ -18,6 +17,8 @@ if path not in sys.path:
 path = "/home/casper50206/mblog"
 if path not in sys.path:
     sys.path.insert(0, path)
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myAssistant.settings')
 
